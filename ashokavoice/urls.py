a0c31c468f6b9f70658a38c23aa86fe3f85f0492
aresly <ilianamacias67@gmail.com>
2024-06-29 17:15:25 -0500

    Commit listarlogro

--- ashokavoice/urls.py
+++ ashokavoice/urls.py
@@ -33,7 +33,7 @@ urlpatterns = [
     path('post/logro/', LogroCreateView.as_view(), name='CreateLogro'),
     path('put/logro/<int:logro_id>', EditLogro.as_view(), name='UpdateLogro'),
     path('delete/logro/<int:logro_id>', EditLogro.as_view(), name='DeleteLogro'),
-    
+    path('get/logro/', EditLogro.as_view(), name='ListarLogro'),
 
 
 ]
